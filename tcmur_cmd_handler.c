e396fb6ebca028ac014c5cc905f55047779d570d
Xiubo Li <lixiubo@cmss.chinamobile.com>
2017-05-06 14:53:24 +0800

    RESERVE/RELEASE
    Signed-off-by: Xiubo Li <lixiubo@cmss.chinamobile.com>

--- tcmur_cmd_handler.c
+++ tcmur_cmd_handler.c
@@ -594,6 +594,9 @@ static int handle_generic_cmd(struct tcmu_device *dev, struct tcmulib_cmd *cmd)
 	uint64_t num_lbas = tcmu_get_dev_num_lbas(dev);
 
 	switch (cdb[0]) {
+	case RESERVE:
+	case RELEASE:
+		return 0;
 	case INQUIRY:
 		return tcmu_emulate_inquiry(dev, cdb, iovec, iov_cnt, sense);
 	case TEST_UNIT_READY:
@@ -635,6 +638,8 @@ static bool command_is_generic(struct tcmulib_cmd *cmd)
 	uint8_t *cdb = cmd->cdb;
 
 	switch(cdb[0]) {
+	case RESERVE:
+	case RELEASE:
 	case INQUIRY:
 	case TEST_UNIT_READY:
 	case MODE_SENSE:
